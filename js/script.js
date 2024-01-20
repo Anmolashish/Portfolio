90f413f6a2b36d1be5497e6719eab048542b3d03
Anmol aashish <aabatwal20@gmail.com>
2024-01-20 20:04:15 +0530

    Made some correction in AOS animations

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
-
+window.addEventListener("resize", () => {
+  const innerAbout = document.getElementById("inner-about");
+  if (window.innerWidth <= 768) {
+    innerAbout.removeAttribute("data-aos");
+    innerAbout.removeAttribute("data-aos-anchor-placement");
+  } else {
+    // Optionally add the attributes back for larger screens
+    innerAbout.setAttribute("data-aos", "fade-up");
+    innerAbout.setAttribute("data-aos-anchor-placement", "bottom-bottom");
+  }
+});
