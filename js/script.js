24fdf10608dc1ee2485439dc962d5a6f17661f63
Anmol aashish <aabatwal20@gmail.com>
2024-01-22 22:18:34 +0530

    Added some changes in color and things

--- js/script.js
+++ js/script.js
@@ -18,7 +18,8 @@ TxtType.prototype.tick = function () {
     this.txt = fullTxt.substring(0, this.txt.length + 1);
   }
 
-  this.el.innerHTML = '<span class="wrap">' + this.txt + "</span>";
+  this.el.innerHTML =
+    '<span class="wrap" style="color:#706c61">' + this.txt + "</span>";
 
   var that = this;
   var delta = 200 - Math.random() * 100;
