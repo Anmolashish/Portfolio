c6ce2d8317088c4877a99d6e557625cd875527a9
Anmol aashish <aabatwal20@gmail.com>
2024-01-22 22:13:52 +0530

    Gave the Website final endings

--- js/script.js
+++ js/script.js
@@ -1,27 +1,58 @@
-let cards = document.querySelectorAll(".card");
-let text = document.getElementById("identity");
-cards.forEach((card) => {
-  card.addEventListener("mouseover", () => {
-    if (card.className == "card one") {
-      text.innerText = "Portfolio";
+var TxtType = function (el, toRotate, period) {
+  this.toRotate = toRotate;
+  this.el = el;
+  this.loopNum = 0;
+  this.period = parseInt(period, 10) || 2000;
+  this.txt = "";
+  this.tick();
+  this.isDeleting = false;
+};
+
+TxtType.prototype.tick = function () {
+  var i = this.loopNum % this.toRotate.length;
+  var fullTxt = this.toRotate[i];
+
+  if (this.isDeleting) {
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
+  } else {
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
+  }
+
+  this.el.innerHTML = '<span class="wrap">' + this.txt + "</span>";
+
+  var that = this;
+  var delta = 200 - Math.random() * 100;
+
+  if (this.isDeleting) {
+    delta /= 2;
+  }
+
+  if (!this.isDeleting && this.txt === fullTxt) {
+    delta = this.period;
+    this.isDeleting = true;
+  } else if (this.isDeleting && this.txt === "") {
+    this.isDeleting = false;
+    this.loopNum++;
+    delta = 500;
+  }
+
+  setTimeout(function () {
+    that.tick();
+  }, delta);
+};
+
+window.onload = function () {
+  var elements = document.getElementsByClassName("typewrite");
+  for (var i = 0; i < elements.length; i++) {
+    var toRotate = elements[i].getAttribute("data-type");
+    var period = elements[i].getAttribute("data-period");
+    if (toRotate) {
+      new TxtType(elements[i], JSON.parse(toRotate), period);
     }
-    if (card.className == "card two") {
-      text.innerText = "Restaurant";
-    }
-    if (card.className == "card three") {
-      text.innerText = "Tic-Tac-Toe Game";
-    }
-    if (card.className == "card four") {
-      text.innerText = "Music Player";
-    }
-    if (card.className == "card five") {
-      text.innerText = "Callorie Counter";
-    }
-    if (card.className == "card six") {
-      text.innerText = "Palindrome Checker";
-    }
-  });
-  card.addEventListener("mouseout", () => {
-    text.innerText = "";
-  });
-});
+  }
+  // INJECT CSS
+  var css = document.createElement("style");
+  css.type = "text/css";
+  css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+  document.body.appendChild(css);
+};
